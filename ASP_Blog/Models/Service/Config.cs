3503c85946314dabc621c29f692fabaa21e9a58f
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2020-08-14 01:39:16 +0300

    Добавьте файлы проекта.

---
+++ ASP_Blog/Models/Service/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP_Blog.Models.Config
+{
+    public class Config
+    {
+        public static string ASP_Blog_UsersDB { get; set; }
+        public static string WebsiteName { get; set; }
+        public static string AllowedUserNameCharacters { get; set; }
+    }
+}
